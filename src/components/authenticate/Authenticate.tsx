cbf3e150ec49c8d76be5d88b1299be336a85a5c5
OMID ME <zabuzara@yahoo.com>
2023-06-09 14:09:57 +0200

    replace auto suggested words in textarea done

--- src/components/authenticate/Authenticate.tsx
+++ src/components/authenticate/Authenticate.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { isValidValue, logger, replaceErrorWithOk } from "../../helpers/functions";
+import { get, isValidValue, logger, replaceErrorWithOk } from "../../helpers/functions";
 import { Mnemonic, Wordlist, ethers } from "ethers";
 import { WORDS } from "./words";
 
@@ -8,6 +8,8 @@ export default function SignIn () {
     const [password, setPassword] = useState("");
     const [suggestionList, setSuggestionList] = useState<string[]>([]);
     const [searchVal, setSearchVal] = useState("");
+    const [valStart, setValStart] = useState(0);
+    const [valEnd, setValEnd] = useState(0);
 
     const suggestion = (value: string) => {
         setSearchVal(value);
@@ -66,12 +68,17 @@ export default function SignIn () {
                             <div 
                                 key={i * 5}
                                 onClick={(e: any) => {
-                                    const appendedContent = phrase.substring(0, phrase.lastIndexOf(' '))
-                                                            .concat((phrase.split(' ').length > 1 ? ' ' : ''))
-                                                            .concat(e.currentTarget.getAttribute('content'))
-                                                            .concat(' ');
-                                    setPhrase(appendedContent);
-                                    suggestion('');
+                                    const phraseTextarea: any = get('.phrase-textarea');
+                                    if (phraseTextarea !== null) {
+                                        const beforeValue = phraseTextarea.value.substring(0, valStart);
+                                        const afterValue = phraseTextarea.value.substring(valEnd);
+                                        const appendedContent = beforeValue.concat(e.currentTarget.getAttribute('content'))
+                                                                           .concat(afterValue);
+                                        setPhrase(appendedContent);
+                                        suggestion('');
+                            
+                                        phraseTextarea.focus();
+                                    }
                                 }}
                                 content={w}
                             >
@@ -87,14 +94,21 @@ export default function SignIn () {
                     placeholder="A mnemonic phrase could have 12, 15, 18, 21 or 24 words. Please Enter and separate them with space."
                     onInput={
                         (event: any) => {
+                            const valueStart = event.target.value.lastIndexOf(' ', event.target.selectionStart - 1) + 1; //event.target.value.substring(event.target.value.lastIndexOf(' ', event.target.selectionStart - 1) + 1);
+                            const valueEnd = valueStart + event.target.value.substring(valueStart).indexOf(' ');
+
+                            setValStart(valueStart);
+                            setValEnd(valueEnd);
+
                             if(isValidValue(event.target, event.target.value)){
                                 setPhrase(event.target.value);
                             } else {
                                 setPhrase("");
                             }
-                            suggestion(event.target.value.substring(event.target.value.lastIndexOf(' ') + 1).toLowerCase());
+                            suggestion(event.target.value.substring(valueStart, valueEnd));
                         }
                     }
+                    className="phrase-textarea"
                     value={phrase}
                 ></textarea>
                 <input 
