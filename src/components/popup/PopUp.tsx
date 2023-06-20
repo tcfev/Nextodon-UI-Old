0aed03044b450ca7e9f5269fb0c5da3b7423bf66
OMID ME <zabuzara@yahoo.com>
2023-06-20 03:38:27 +0200

    popup component color changed

--- src/components/popup/PopUp.tsx
+++ src/components/popup/PopUp.tsx
@@ -8,12 +8,12 @@ type PopUpProperties = {
 export default function PopUp (props: PopUpProperties) {
     return (
         <div className="popup-box">
-            <dialog className="border rounded p-5 shadow col m-auto" open>
+            <dialog className="border-secondary rounded p-5 col m-auto" open>
                 <p className="row mb-5">{props.message}</p>
                 <form className="row" method="dialog">
                     <div className="row gap-4 m-auto">
-                        <button className="col px-2 btn btn-danger" value="cancel" formMethod="dialog" onClick={() => props.noCallback(true)}>Cancel</button>
-                        <button className="col px-2 btn btn-success" id="confirmBtn" value="default" onClick={() => {
+                        <button className="col px-2 btn btn-outline-secondary" value="cancel" formMethod="dialog" onClick={() => props.noCallback(true)}>Cancel</button>
+                        <button className="col px-2 btn btn-primary text-" id="confirmBtn" value="default" onClick={() => {
                             props.noCallback(true)
                             props.yesCallback(false);
                         }}>Confirm</button>
