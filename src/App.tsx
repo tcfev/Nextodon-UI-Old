5d7cd4b6ab8ce909a84ac24005280d2defbe424e
OMID ME <zabuzara@yahoo.com>
2023-07-04 22:19:48 +0200

    pwa dark light done

--- src/App.tsx
+++ src/App.tsx
@@ -19,8 +19,8 @@ function App() {
 
   return (
     <div className="App container flex-column justify-content-center py-2" onContextMenu={handleOnContextMenu}>
-      <div className='col-lg-6 col-md-8 col-sm-10 mx-auto mt-5'>
-        <div className='d-flex justify-content-center'>
+      <div className='col-lg-6 col-md-8 col-sm-10 mx-auto mt-3'>
+        <div className='d-flex justify-content-center mb-3'>
           <img className="p-1 align-self-center mb-2" alt="logo" width="40" height="40" src={require('./assets/media/logo-48.png')}/> 
           <h1 className="p-2 align-self-center fw-bold text-center mb-2" style={{color:'#5b46db'}}>
             Nextodon
